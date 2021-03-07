4aa7cb77c3786eeb17b0dddf9d35c270c455e011
bzz <micanovicm@bzz.ch>
2021-03-07 12:41:56 +0100

    Klasse "User" fertig

--- src/main/java/ch/bzz/onlineshop/model/User.java
+++ src/main/java/ch/bzz/onlineshop/model/User.java
@@ -1,30 +1,61 @@
 package ch.bzz.onlineshop.model;
 
+/**
+ * user class
+ * <p>
+ * Onlineshop
+ *
+ * @author Marko Micanovic
+ */
 public class User {
     private String userUUID;
     private String password;
     private String userName;
 
+    /**
+     * gets the uuid from the user
+     * @return uuid from user
+     */
     public String getUserUUID() {
         return userUUID;
     }
 
+    /**
+     * sets the uuid from the user
+     * @param userUUID
+     */
     public void setUserUUID(String userUUID) {
         this.userUUID = userUUID;
     }
 
+    /**
+     * gets the password from the user
+     * @return password from user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * sets the password from the user
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * gets the username from the user
+     * @return username from user
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * sets the username from the user
+     * @param userName
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
