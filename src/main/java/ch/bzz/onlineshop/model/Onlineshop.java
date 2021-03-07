31d839dbace4035003706f6de2861689bc0563e8
bzz <micanovicm@bzz.ch>
2021-03-07 12:38:41 +0100

    Klasse "Onlineshop" fertig

--- src/main/java/ch/bzz/onlineshop/model/Onlineshop.java
+++ src/main/java/ch/bzz/onlineshop/model/Onlineshop.java
@@ -2,44 +2,88 @@ package ch.bzz.onlineshop.model;
 
 import java.util.ArrayList;
 
+/**
+ * a onlineshop of a article
+ * <p>
+ * Onlineshop
+ *
+ * @author Marko Micanovic
+ */
 public class Onlineshop {
     private ArrayList<Artikel> alleArtikel;
     private String url;
     private int anzahlRegistrierteBenutzer;
     private int anzahlBestellungen;
 
+    /**
+     * Gets the artikelMap
+     * @return map of articles
+     */
     public ArrayList<Artikel> getAlleArtikel() {
         return alleArtikel;
     }
 
+    /**
+     * sets the artikelMap
+     * @param alleArtikel
+     */
     public void setAlleArtikel(ArrayList<Artikel> alleArtikel) {
         this.alleArtikel = alleArtikel;
     }
 
+    /**
+     * gets a single article by index
+     * @param index
+     * @return single article
+     */
     public Artikel getArtikel(int index){
         return alleArtikel.get(index);
     }
 
+    /**
+     * gets the onlineshop-url
+     * @return onlineshop-url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * sets the onlineshop-url
+     * @param url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * gets the number of registered users
+     * @return number of registered users
+     */
     public int getAnzahlRegistrierteBenutzer() {
         return anzahlRegistrierteBenutzer;
     }
 
+    /**
+     * sets the number of registered users
+     * @param anzahlRegistrierteBenutzer
+     */
     public void setAnzahlRegistrierteBenutzer(int anzahlRegistrierteBenutzer) {
         this.anzahlRegistrierteBenutzer = anzahlRegistrierteBenutzer;
     }
 
+    /**
+     * gets the number of completed orders
+     * @return number of completed orders
+     */
     public int getAnzahlBestellungen() {
         return anzahlBestellungen;
     }
 
+    /**
+     * sets the number of completed orders
+     * @param anzahlBestellungen
+     */
     public void setAnzahlBestellungen(int anzahlBestellungen) {
         this.anzahlBestellungen = anzahlBestellungen;
     }
